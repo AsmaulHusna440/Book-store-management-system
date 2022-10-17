b6413eab794b85a416fa63cb4e106141c3b4f0de
AsmaulHusna440 <110192100+AsmaulHusna440@users.noreply.github.com>
2022-10-17 21:40:32 +0600

    Add files via upload

---
+++ bookstore.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 21, 2022 at 09:43 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_user`
+--
+
+CREATE TABLE `admin_user` (
+  `id` int(11) NOT NULL,
+  `admin_id` varchar(50) NOT NULL,
+  `password` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_user`
+--
+
+INSERT INTO `admin_user` (`id`, `admin_id`, `password`) VALUES
+(1, '1', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_user`
+--
+ALTER TABLE `admin_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_user`
+--
+ALTER TABLE `admin_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ signin_backend.php
@@ -0,0 +1,51 @@
+<?php
+$phoneError = $passwordError = $errorText = "";
+$phone = $password= "";
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "bookstore";
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["phone"])) {
+		$phoneError = "Phone is required";
+	} else {
+		$phone = test_input($_POST["phone"]);
+	}
+
+	if (empty($_POST["password"])) {
+		$passwordError = "Password is required";
+	} else {
+		$password = test_input($_POST["password"]);
+	}
+	if( strlen($phoneError) == 0 && strlen($passwordError) == 0 ) {
+		// Ready to login
+		$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+		if (mysqli_connect_error()){
+			$errorText=('Connect Error ('. mysqli_connect_errno() .') ' . mysqli_connect_error());
+		}
+		else {
+			$sql = "select phone, password from user where(phone='$phone'and password='$password')";
+			$result=$conn->query($sql);
+			if ($result->num_rows>0){
+				while($row=$result->fetch_assoc()){
+					$uphone=$row["phone"];
+					$upassword=$row["password"];
+					#echo "<br>Username: ".$row["phone"]."<br>Password: ".$row["password"]."<br>";
+				}
+				$conn->close();
+				header("Location: gallery.html");
+			}
+			else{
+				$conn->close();
+				$errorText= "Wrong Username or Password!";
+			}
+		}
+	}
+}
+?>
